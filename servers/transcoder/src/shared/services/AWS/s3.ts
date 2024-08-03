79851e4be5d827e87e06bc826ff02de7e5ee78f6
Antar <antardas2334@gmail.com>
2024-08-03 17:05:16 +0600

    fix: localstack error

--- servers/transcoder/src/shared/services/AWS/s3.ts
+++ servers/transcoder/src/shared/services/AWS/s3.ts
@@ -1,10 +1,14 @@
 import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+dotenv.config({path: '.env'})
 
 export const s3 = new AWS.S3({
 	endpoint: process.env.AWS_ENDPOINT,
-	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? 'TEST_ACCESS_KEY',
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? 'TEST_SECRET_ACCESS_KEY',
 	region: process.env.AWS_REGION,
+	s3ForcePathStyle: true,
+
 });
 
 export const BUCKET_NAME = process.env.AWS_S3_BUCKET || 'video';
