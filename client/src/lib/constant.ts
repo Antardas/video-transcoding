dae36ca2f6c49e204ecd6229b3a9fa7d357cb13e
Antar <antardas2334@gmail.com>
2024-08-09 17:50:55 +0600

    fix: change service server url

--- client/src/lib/constant.ts
+++ client/src/lib/constant.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'http://localhost:5002';
-export const WATCH_SERVER_URL = 'http://localhost:5004'
+export const SERVER_URL = 'http://localhost:5000';
+export const WATCH_SERVER_URL = 'http://localhost:5000'
